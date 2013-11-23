228bc525cfc7c8ffbae3cea12efcbd797bd119fd
Jasmine Hsu <jasminechsu@gmail.com>
2013-11-23 16:58:55 -0500

    final

---
+++ GlassTutorial/src/hack/virginia/glasstutorial/GetLocation.java
@@ -0,0 +1,89 @@
+package hack.virginia.glasstutorial;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class GetLocation extends Activity implements LocationListener {
+  private TextView latituteField;
+  private TextView longitudeField;
+  private LocationManager locationManager;
+  private String provider;
+  String gps="";
+
+ 
+/** Called when the activity is first created. */
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Get the location manager
+    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    // Define the criteria how to select the locatioin provider -> use
+    // default
+    Criteria criteria = new Criteria();
+    provider = locationManager.getBestProvider(criteria, false);
+    Location location = locationManager.getLastKnownLocation(provider);
+
+    // Initialize the location fields
+    if (location != null) {
+      System.out.println("Provider " + provider + " has been selected.");
+      onLocationChanged(location);
+
+    } else {
+      gps = "Location not available";
+
+    }
+  }
+  public String getLoc() {
+	return gps;
+
+  }
+  /* Request updates at startup */
+  @Override
+  protected void onResume() {
+    super.onResume();
+    locationManager.requestLocationUpdates(provider, 400, 1, this);
+  }
+
+  /* Remove the locationlistener updates when Activity is paused */
+  @Override
+  protected void onPause() {
+    super.onPause();
+    locationManager.removeUpdates(this);
+  }
+
+  @Override
+  public void onLocationChanged(Location location) {
+    float lat = (float) (location.getLatitude());
+    float lng = (float) (location.getLongitude());
+    gps = lat+";"+lng;
+  }
+
+@Override
+public void onProviderDisabled(String provider) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void onProviderEnabled(String provider) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void onStatusChanged(String provider, int status, Bundle extras) {
+	// TODO Auto-generated method stub
+	
+}
+
+} 
---
+++ GlassTutorial/src/hack/virginia/glasstutorial/ImageTweetedEvent.java
@@ -0,0 +1,4 @@
+package hack.virginia.glasstutorial;
+
+public class ImageTweetedEvent {
+}
