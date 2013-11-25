287c00fd05cf66ecb94282f8a66d7a3810bb9091
Jasmine Hsu <jasminechsu@gmail.com>
2013-11-25 11:32:25 -0500

    glassteam module

--- firstglanceglass/src/hack/virginia/glassteam/TweetIntentService.java
+++ firstglanceglass/src/hack/virginia/glassteam/TweetIntentService.java
@@ -14,6 +14,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.text.TextUtils;
 import android.util.Log;
+import hack.virginia.glassteam.App;
 
 
 public class TweetIntentService extends IntentService {
